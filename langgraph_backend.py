58ac69db1499eb7ef0f121a867c7bc9828702fc9
Lalit Pathak <lalitpathak009@gmail.com>
2026-04-17 16:35:50 +0530

    Update langgraph_backend.py

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -3,11 +3,19 @@ from typing import TypedDict, Annotated
 from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END, add_messages
 from langgraph.checkpoint.sqlite import SqliteSaver
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, AnyMessage
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.tools import tool
+from langchain_community.tools import DuckDuckGoSearchRun
 from dotenv import load_dotenv
 import sqlite3
 
 DB_PATH = "chatbot_memory.db"
+
+# ===================== Model Initialization =====================
+load_dotenv()
+model = ChatGroq(model="llama-3.1-8b-instant")
+
 
 # ===================== Thread Registry =====================
 
@@ -39,15 +47,59 @@ def retrieve_all_threads():
     conn.close()
     return [r[0] for r in rows]
 
+# ===================== Tools =====================
+
+
+#search_tool = DuckDuckGoSearchRun(region="us", safesearch="Off")
+@tool
+def web_search(query: str):
+    """Search the web for current events."""
+    search_tool = DuckDuckGoSearchRun(region="us", safesearch="Off")
+    result = search_tool.run(query)
+    return result
+
+@tool
+def calculator_tool(first: float, second: float, operation: str) -> dict:
+    """Performs basic arithmetic operations on two numbers. Supported operations are: add, subtract, multiply, divide."""
+    if operation == "add":
+        return first + second
+    elif operation == "subtract":
+        return first - second
+    elif operation == "multiply":
+        return first * second
+    elif operation == "divide":
+        return first / second
+    else:
+        raise ValueError("Unsupported operation")
+    
+
+all_tools = [web_search, calculator_tool]
+
+
+
+model_with_tools = model.bind_tools(all_tools)
 # ===================== LangGraph State =====================
 
 class ChatState(TypedDict):
-    history: Annotated[list[BaseMessage], add_messages]
+    history: Annotated[list[AnyMessage], add_messages]
 
 def chat_node(state: ChatState):
-    model = ChatGroq(model="llama-3.1-8b-instant")
-    response = model.invoke(state["history"])
+    response = model_with_tools.invoke(state["history"])
     return {"history": [response]}
+
+tools_node = ToolNode(all_tools, messages_key='history')
+
+
+def custom_tools_condition(state: ChatState):
+    last_message = state["history"][-1]
+
+    if getattr(last_message, "tool_calls", None):
+        return "tools_node"
+
+    if "tool_calls" in getattr(last_message, "additional_kwargs", {}):
+        return "tools_node"
+
+    return "__end__"
 
 # ===================== Graph =====================
 
@@ -58,8 +110,11 @@ def get_chatbot():
 
     workflow = StateGraph(ChatState)
     workflow.add_node("chat_node", chat_node)
+    workflow.add_node("tools_node", tools_node)
+    
     workflow.add_edge(START, "chat_node")
-    workflow.add_edge("chat_node", END)
+    workflow.add_conditional_edges("chat_node", custom_tools_condition)
+    workflow.add_edge("tools_node", 'chat_node')
 
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     memory = SqliteSaver(conn=conn)
